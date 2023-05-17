987136b24fd6d94d4193388830197d3c45a0c4aa
Hugo Alberto Martínez Ortega <humartinez92@outlook.com>
2023-05-17 13:55:41 -0500

    Se crean las regiones OPCIONES DE USO y SECCIÓN A - Información del Contribuyente.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario/app.formulario.html',
+  styleUrls: ['./formulario/app.formulario.css']
+})
+export class AppComponent {
+  title = 'fun_dec_ica';
+}
